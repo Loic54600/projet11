99bad1280d3e787a6e4d164aa36e39150c7c8fe7
loic guervin <odboy_54600@hotmail.fr>
2024-04-19 12:54:14 +0200

    img opti

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import Logo from '../../../src/assets/argentBankLogo.png'
+import Logo from '../../../src/assets/argentBankLogo.webp'
 
 const Header = () => {
   return (
