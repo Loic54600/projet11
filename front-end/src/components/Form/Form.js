a13f62540ff501c308321f164f3dee58b7dabcbe
loic guervin <odboy_54600@hotmail.fr>
2024-05-10 09:55:18 +0200

    recup

--- front-end/src/components/Form/Form.js
+++ front-end/src/components/Form/Form.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+function FormSignin () {
 
+
+
+
+  
+}
 
 const Form = () => {
   return (
--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-
 import Footer from '../../components/Footer/Footer';
+import Form  from '../../components/Form/Form';
 import Header from '../../components/Header/Header';
 
 
 const Login = () => {
-
-  
-
-
   return (
     <main>
       <Header />
       <main className='main bg-dark'>
-        <section className="sign-in-content">
-          <i className="fa fa-user-circle sign-in-icon"></i>
-          <h1>Sign In</h1>
-          <form>
-            <div className="input-wrapper">
-              <label for="username">Username</label>
-              <input type="text" id="username" />
-            </div>
-            <div className="input-wrapper">
-              <label for="password">Password</label>
-              <input type="password" id="password" />
-            </div>
-            <div className="input-remember">
-              <input type="checkbox" id="remember-me" /><label for="remember-me">Remember me</label>
-            </div>
-            <NavLink to="/profil" >
-              <div className="sign-in-button">Sign In</div>
-            </NavLink>
-          </form>
-        </section>
+        <Form />
       </main>
       <Footer />
     </main>
