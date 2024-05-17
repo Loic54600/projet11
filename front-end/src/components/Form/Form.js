4a0d5fc503db9cc497c00a0d41735068c78e7ce9
loic guervin <odboy_54600@hotmail.fr>
2024-05-17 13:29:44 +0200

    connection

--- front-end/src/components/Form/Form.js
+++ front-end/src/components/Form/Form.js
@@ -1,36 +1,103 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate, useDispatch  } from 'react-router-dom';
 
 function FormSignin () {
+  const navigate = useNavigate ();
+  const dispatch = useDispatch();
+  const loginButton = useRef();
+
+  const [rememberMe, setRememberMe] = useState(false);
 
 
+  async function loginRequest() {
+    /*Creation de la variable du formulaire
+    - on récupére la valeur email et password */
+    let Formlogin = {
+      "email": usernameInput.current.value,
+      "password": passwordInput.current.value,
+    };
+
+    let chargeUtile = JSON.stringify(Formlogin);
+
+    return await fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: chargeUtile,
+    }).then((resp) => resp.json());
+  }
+
+/*Creation de la variable de connexion*/
+const handleLogin = async (event) => {
+  event.preventDefault();
+  try {
+    /*variable reponse  */
+    const response = await loginRequest();
+    /*si reponse est une erreur renvoie Echec  */
+    if (response.error) {
+      Echec();
+      /*sinon la connexion s'effectue */
+    } else {
+      const token = response.body.token;
+      sessionStorage.setItem("token", token);
+      if (rememberMe) {
+        localStorage.setItem("token", token);
+      }
+      dispatch({ type: "LOGIN_SUCCESS" });
+      navigate("/profile");
+    }
+  } catch (error) {
+    Echec();
+  }
+};
+/* 1- Tout d’abord, le code dans try {...} est exécuté
+   2- S’il n’y a pas eu d’erreur, alors catch(error) est ignoré : l’exécution arrive à la fin de try et continue en ignorant catch.
+   3- Si une erreur survient, alors l’exécution de try est arrêtée et le contrôle se place au début de catch(error).
+*/
+
+/*Fonction Echec de connexion */
+  function Echec() {
+    /*active la classe en echec */
+    passwordInput.current.classList.add("Failed");
+    usernameInput.current.classList.add("Failed");
+    window.setTimeout(function () {
+      /*Annule la classe */
+      passwordInput.current.classList.remove("Failed");
+      usernameInput.current.classList.remove("Failed");
+    }, 500);
+  }
 
 
-  
-}
-
-const Form = () => {
   return (
+    <main className="main bg-dark">
     <section className="sign-in-content">
       <i className="fa fa-user-circle sign-in-icon"></i>
       <h1>Sign In</h1>
-      <form>
+      <form onSubmit={handleLogin}>
         <div className="input-wrapper">
-          <label for="username">Username</label>
-          <input type="text" id="username" />
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" required ref={usernameInput} />
         </div>
         <div className="input-wrapper">
-          <label for="password">Password</label>
-          <input type="password" id="password" />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" required ref={passwordInput} />
         </div>
         <div className="input-remember">
-          <input type="checkbox" id="remember-me" /><label for="remember-me">Remember me</label>
+          <input
+            type="checkbox"
+            id="remember-me"
+            onChange={(e) => setRememberMe(e.target.checked)}
+          />
+          <label htmlFor="remember-me">Remember me</label>
         </div>
-        <NavLink to="/profil" >
-          <div className="sign-in-button">Sign In</div>
-        </NavLink>
+        <button className="sign-in-button"id="sign-in-button"
+          ref={loginButton}
+          onClick={handleLogin}
+        >
+          Sign In
+        </button>
       </form>
     </section>
-  );
-};
-export default Form;
+  </main>
+  )
+}
+export default FormSignin;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 
 import "../src/styles/index.scss";
 
-import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+
 
 import Home from '../src/pages/Home/Home';
 import Login from '../src/pages/Login/Login';
-import Profil from '../src/pages/Profile/Profil';
+import Profile from './pages/Profile/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,7 +18,7 @@ root.render(
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profil" element={<Profil />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
